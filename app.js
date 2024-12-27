f869e2200ff85307185111d90e75b70fae712431
Ola Khateeb <ola.khateeb29@gmail.com>
2024-12-27 16:33:30 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const dirPath = path.join(__dirname, "/templates");
+
+const file2Send = fs.readFileSync(`${dirPath}/page.html`);
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.end(file2Send);
+});
+
+server.listen(3000, () => {
+  console.log("Server working on port 3000");
+});
